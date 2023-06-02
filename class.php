8d82ea0810cbce96337fe0b1966546900b72c40c
nagachen <birdann99@gmail.com>
2023-06-02 14:04:04 +0800

    物件導向初探

---
+++ class.php
@@ -0,0 +1,23 @@
+<?php
+class Animal{
+    private  $age;
+    public $name;
+    protected $hair;
+
+    function ——construct(){   //建構式
+
+    }
+    public function age(){   //預設為public 
+        
+    }
+    private function speed(){
+
+    }
+    protected function hair(){
+
+    }
+}
+
+$age;
+
+?>
