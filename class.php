1dd1b9d64f633e0cf4494dcb7812b03787faf90e
nagachen <birdann99@gmail.com>
2023-06-02 14:33:40 +0800

    物件導向，方法屬性的使用和權限的說明

--- class.php
+++ class.php
@@ -1,23 +1,40 @@
 <?php
 class Animal{
-    private  $age;
-    public $name;
-    protected $hair;
+    private  $age=10;
+    protected $name="大雄";
+    protected $hair="red";
 
-    function __construct(){   //建構式
+    function __construct(){   //建構式,建立物件馬上執行的函式
+        echo "建立物件";
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
 
+    public function setName($string){
+        return $this->name;
     }
     public function age(){   //預設為public 
-        
+        return $this->age;
     }
     private function speed(){
-
+        return "跑速1000";
     }
-    protected function hair(){
-
+    protected function run(){
+        return "跑跑跑";
     }
 }
+//生成物件
+$animal=new Animal;
+echo"<br>";
+echo $animal->getName();
+// echo $animal->age;  //無法存取
+// echo $animal->hair; //無法存取
+$animal->setName("小莉");  
 
-$age;
+echo $animal->age()   //讀取private屬性的方式 Get的方式
+
+// echo $animal->speed();
 
 ?>
